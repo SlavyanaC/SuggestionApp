fd571f181c9b858f863b4bfa7891ac3b0527dfd7
Slavyana Chonovska <slaviana95@gmail.com>
2022-01-24 15:57:10 +0200

    Add Blazor Server and Class Library projects with Basic Models

---
+++ SuggestionAppLibrary/Models/BasicSuggestionModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SuggestionAppLibrary.Models;
+
+public class BasicSuggestionModel
+{
+    public BasicSuggestionModel() { }
+
+    public BasicSuggestionModel(SuggestionModel suggestion)
+    {
+        Id = suggestion.Id;
+        Suggestion = suggestion.Suggestion;
+    }
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Suggestion { get; set; }
+}
---
+++ SuggestionAppLibrary/Models/BasicUserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SuggestionAppLibrary.Models;
+
+public class BasicUserModel
+{
+    public BasicUserModel() { }
+
+    public BasicUserModel(UserModel user)
+    {
+        Id = user.Id;
+        DisplayName = user.DisplayName;
+    }
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string DisplayName { get; set; }
+}
